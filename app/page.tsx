0c6db334594fa705f6dac9d48a6b90fddef60eaa
Krish <krishnerkar@gmail.com>
2023-11-23 13:16:34 +0530

    Add basic pages and layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-12 px-16">
-      <Navbar/>
+    <main>
+      <div>Main Page</div>
     </main>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,18 +8,29 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="bg-gray-200 flex justify-between items-center w-full rounded-md py-4 px-8">
-      <h3 className="text-lg font-bold">Mini Project</h3>
+      <h3 className="text-lg font-bold">
+        <Link href="/">Mini Project</Link>
+      </h3>
       <Menubar>
         <MenubarMenu>
           <MenubarTrigger className="cursor-pointer">
             <HamburgerMenuIcon />
           </MenubarTrigger>
           <MenubarContent align="end">
-            <MenubarItem>Dashboard</MenubarItem>
+            <Link href="/designer">
+              <MenubarItem>Designer</MenubarItem>
+            </Link>
+            <Link href="/manufacturer">
+              <MenubarItem>Manufacturer</MenubarItem>
+            </Link>
+            <Link href="/supplier">
+              <MenubarItem>Supplier</MenubarItem>
+            </Link>
           </MenubarContent>
         </MenubarMenu>
       </Menubar>
